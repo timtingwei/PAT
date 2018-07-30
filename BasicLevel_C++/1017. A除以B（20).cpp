2f88eeb2545d2a792d9bf10a0c525bbc9a3ef390
timtingwei <timtingwei@hotmail.com>
2018-07-31 00:27:45 +0800

    修改 更省空间和减少循环次数的做法 - 模拟题

--- BasicLevel_C++/1017. A除以B（20).cpp
+++ BasicLevel_C++/1017. A除以B（20).cpp
@@ -1,7 +1,10 @@
 // Copyright [2018] <mituh>
 // 1017. A除以B（20).cpp    // 3:25  (30) 4:00 -> 35min
 /*
-  思路: 用数组储存大整数, 模拟大整数除以1位数
+  思路:
+  1, 用数组储存大整数, 模拟大整数除以1位数
+  2, 用C++字符串读取, 模拟手动除法, 首位整数 / B, 如果得到不是0就输出, 余数*10+下一位,
+  
 
 本题要求计算 A/B，其中 A 是不超过 1000 位的正整数，B 是 1 位正整数。你需要输出商数 Q 和余数 R，使得 A=B×Q+R 成立。
 
@@ -11,11 +14,10 @@
 输出样例：
 17636684150141093474 3
 */
+
+/*
 #include <cstdio>
 #include <cstring>
-#include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 char bs[1050];
@@ -36,5 +38,29 @@ int main() {
   if (cnt == 0 && ans[0] == 0) printf("0");
   for (int i = 0; i < cnt; i++) { printf("%d", ans[i]); }
   printf(" %d\n", r);
+  return 0;
+}
+
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+  string a; int b;
+  cin >> a >> b;
+  int len = a.length();
+  int f = (a[0] - '0') / b;
+  if ((f != 0 && len > 1)|| len == 1)
+    cout << f;
+  int temp = (a[0] - '0') % b;
+  for (int i = 1; i < len; i++) {
+    f = (temp * 10 + a[i] - '0') / b;
+    cout << f;
+    temp = (temp * 10 + a[i] - '0') % b;
+  }
+  cout << " " << temp << "\n";
   return 0;
 }
