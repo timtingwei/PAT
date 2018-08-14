a318f728df6bc823f21b132927466b0f2807a826
timtingwei <timtingwei@hotmail.com>
2018-08-15 02:00:28 +0800

    修改成用下标管理数组的版本

---
+++ alg_notes/ch4/4-1/pat-a-1025.cpp
@@ -0,0 +1,102 @@
+// Copyright [2018] <mituh>
+// 1025. PAT Ranking (25).cpp 12:58 -> 01:23
+/*
+1025 PAT Ranking (25)（25 point(s)）
+
+Programming Ability Test (PAT) is organized by the College of Computer Science and Technology of Zhejiang University. Each test is supposed to run simultaneously in several places, and the ranklists will be merged immediately after the test. Now it is your job to write a program to correctly merge all the ranklists and generate the final rank.
+
+Input Specification:
+
+Each input file contains one test case. For each case, the first line contains a positive number N (<=100), the number of test locations. Then N ranklists follow, each starts with a line containing a positive integer K (<=300), the number of testees, and then K lines containing the registration number (a 13-digit number) and the total score of each testee. All the numbers in a line are separated by a space.
+
+Output Specification:
+
+For each test case, first print in one line the total number of testees. Then print the final ranklist in the following format:
+
+registration_number final_rank location_number local_rank
+
+The locations are numbered from 1 to N. The output must be sorted in nondecreasing order of the final ranks. The testees with the same score must have the same rank, and the output must be sorted in nondecreasing order of their registration numbers.
+
+Sample Input:
+
+2
+5
+1234567890001 95
+1234567890005 100
+1234567890003 95
+1234567890002 77
+1234567890004 85
+4
+1234567890013 65
+1234567890011 25
+1234567890014 100
+1234567890012 85
+Sample Output:
+
+9
+1234567890005 1 1 1
+1234567890014 1 2 1
+1234567890001 3 1 2
+1234567890003 3 1 2
+1234567890004 5 1 4
+1234567890012 5 2 2
+1234567890002 7 1 5
+1234567890013 8 2 3
+1234567890011 9 2 4
+*/
+
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+struct Testee {
+  char id[15];
+  int score;
+  int final_rank;
+  int local_number;
+  int local_rank;
+} stu[100050];
+
+bool cmp(Testee t1, Testee t2) {
+  if (t1.score != t2.score) {
+    return t1.score > t2.score;
+  } else {
+    return strcmp(t1.id, t2.id) < 0;
+  }
+}
+
+int main() {
+  int N, K, num = 0;
+  scanf("%d", &N);
+  for (int in = 0; in < N; in++) {
+    scanf("%d", &K);
+    for (int i = 0; i < K; i++) {
+      scanf("%s %d", stu[num].id, &stu[num].score);
+      stu[num].local_number = in+1;
+      num++;
+    }
+    sort(stu+num-K, stu+num, cmp);
+    int r = 1;
+    stu[num-K].local_rank = 1;
+    for (int i = num-K+1; i < num; i++) {
+      if (stu[i].score == stu[i-1].score) {
+        stu[i].local_rank = stu[i-1].local_rank;
+      } else {
+        stu[i].local_rank = i + 1 - (num-K);
+      }
+    }
+  }
+  sort(stu, stu+num, cmp);
+
+  printf("%d\n", num);
+  int r = 1;
+  for (int i = 0; i < num; i++) {
+    if (i > 0 && stu[i].score != stu[i-1].score) {
+      r = i+1;
+    }
+    printf("%s %d %d %d\n", stu[i].id, r,
+           stu[i].local_number, stu[i].local_rank);
+  }
+  return 0;
+}
