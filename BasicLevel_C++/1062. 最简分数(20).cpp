ff59135ebe0a207be03a85292a7c05a0948e846e
timtingwei <timtingwei@hotmail.com>
2018-08-25 05:17:38 +0800

    Update最大公约数做法

--- BasicLevel_C++/1062. 最简分数(20).cpp
+++ BasicLevel_C++/1062. 最简分数(20).cpp
@@ -19,150 +19,39 @@
 输出样例：
 5/12 7/12
 */
-
-
-/*
+#include <iostream>
 #include <cstdio>
 #include <cmath>
-#include <cstring>
-#include <iostream>
-int limit[1000];
-int mp[2500];
+using namespace std;
+
+int gcd(int a, int b) {
+  return !b ? a : gcd(b, a%b);
+}
 
 int main() {
-  char s1[15], s2[15]; int K;
-  scanf("%s %s %d", s1, s2, &K);
-  getchar();     // print
-  int limit_n = 0;
-// printf("K = %d\n", K);
+  int N1, M1, N2, M2, K;
+  scanf("%d/%d %d/%d %d", &N1, &M1, &N2, &M2, &K);
 
-
-  for (int i = 2; i <= K; i++) {   // bug01, i != 0
-    if (K % i == 0) {
-      limit[limit_n++] = i;
-// printf("i = %d\n", i);
-    }
+  // case1
+  if (N1*M2 > N2*M1) {
+    swap(N1, N2);
+    swap(M1, M2);
   }
 
-  int N1, M1, N2, M2;
-  sscanf(s1, "%d/%d", &N1, &M1);
-  sscanf(s2, "%d/%d", &N2, &M2);
-  memset(mp, 0, sizeof(mp));
-  // int a = N1/(M1/K), b = N2/(M2/K);
-  int a = ceil(     N1/((double)M1/K)   ), b = floor(  N2/( (double) M2/K));
-  printf("a = %d, b = %d\n", a, b);
-  int scale = 1, over = 0;
-
-  while (!over) {
-    over = 1;
-    for (int i = 0; i < limit_n; i++) {
-      int tmp = limit[i] * scale;
-
-      if (tmp <= b) {
-        over = 0;
-        if (a <= tmp) {
-          mp[tmp] = 1;
-          printf("tmp = %d\n", tmp);
-        }
-      }
+  int num = 1;
+  while (num * M1 <= N1 * K) num++;
+  int flag = 0;
+  while (num * M1 > N1 * K && M2 * num < N2 * K) {
+    if (gcd(num, K) == 1) {
+      printf("%s%d/%d", (flag ? " " : ""), num, K);
+      flag = 1;
     }
-    scale++;
-    if (over) break;
+    num++;
   }
-  // getchar();
-  int first = 0;
-  for (int i = a; i <= b; i++) {
-    if (!mp[i]) {
-      if (first) printf(" ");
-      printf("%d/%d", i, K);
-      first = 1;
-    }
-  }
-  printf("\n");
   return 0;
 }
 
-
-// case1, 2, wrong answer
-*/
-
 /*
-#include <cstdio>
-#include <cmath>
-#include <cstring>
-#include <iostream>
-int limit[1000];
-int mp[2500];
-
-int main() {
-  char s1[15], s2[15]; int K;
-  scanf("%s %s %d", s1, s2, &K);
-  getchar();     // print
-  int limit_n = 0;
-// printf("K = %d\n", K);
-
-
-  for (int i = 2; i <= K; i++) {   // bug01, i != 0
-    if (K % i == 0) {
-      limit[limit_n++] = i;
-// printf("i = %d\n", i);
-    }
-  }
-
-  int N1, M1, N2, M2;
-  sscanf(s1, "%d/%d", &N1, &M1);
-  sscanf(s2, "%d/%d", &N2, &M2);
-  memset(mp, 0, sizeof(mp));
-  // int a = N1/(M1/K), b = N2/(M2/K);
-  int a = ceil(     N1/((double)M1/K)   ), b = floor(  N2/( (double) M2/K));
-// printf("a = %d, b = %d\n", a, b);
-  if (a > b) {                    // debug, case1
-    int tmp = a; a = b; b = tmp;
-  }
-
-  if (K == 1) {                   // debug, case1
-    for (int i = a; i <= b; i++) {
-      if (i != a) printf(" ");
-      printf("%d/%d", i, K);
-    }
-    return 0;
-  }
-
-  int scale = 1, over = 0;
-  while (!over) {
-    over = 1;
-    for (int i = 0; i < limit_n; i++) {
-      int tmp = limit[i] * scale;
-
-      if (tmp <= b) {
-        over = 0;
-        if (a <= tmp) {
-          mp[tmp] = 1;
-// printf("tmp = %d\n", tmp);
-        }
-      }
-    }
-    scale++;
-    if (over) break;
-  }
-// getchar();
-  int first = 0;
-  for (int i = a; i <= b; i++) {
-    if (!mp[i]) {
-      if (first) printf(" ");
-      printf("%d/%d", i, K);
-      first = 1;
-    }
-  }
-  printf("\n");
-  return 0;
-}
-
-// 修复case1, K=1的情况, case2 Wrong answer
-*/
-
-
-
 #include <cstdio>
 #include <cmath>
 #include <cstring>
@@ -229,3 +118,4 @@ int main() {
   printf("\n");
   return 0;
 }
+*/
