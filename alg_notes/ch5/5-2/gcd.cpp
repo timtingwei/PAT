689e6efdfd428f2020d145366f898ee9c7c72f7d
timtingwei <timtingwei@hotmail.com>
2018-08-24 22:17:12 +0800

    Create gcd.cpp : greatest common divisor, 最大公约数

---
+++ alg_notes/ch5/5-2/gcd.cpp
@@ -0,0 +1,24 @@
+// Copyright [2018] <mituh>
+// gcd.cpp
+// greatest common divisor, 最大公约数
+#include <cstdio>
+
+/*
+// 循环的写法
+int gcd(int a, int b) {
+  while (b != 0) {
+    int tmp = a; a = b; b = tmp % b;
+  }
+  return a;
+}
+*/
+
+// 递归的写法
+int gcd(int a, int b) {
+  return !b ? a : gcd(b, a % b);
+}
+
+int main() {
+  printf("%d\n", gcd(26, 39));
+  return 0;
+}
