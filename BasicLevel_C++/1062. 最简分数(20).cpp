17da2ae65eff0385626aa118b6eea3065fcd5a66
timtingwei <timtingwei@hotmail.com>
2018-09-07 01:35:02 +0800

    Update 1062

--- BasicLevel_C++/1062. 最简分数(20).cpp
+++ BasicLevel_C++/1062. 最简分数(20).cpp
@@ -19,6 +19,129 @@
 输出样例：
 5/12 7/12
 */
+
+#include <cstdio>
+struct Fraction {
+  long long up, down;
+};
+
+long long gcd(long long a, long long b) {
+  return !b ? a : gcd(b, a % b);
+}
+
+void reduction(Fraction &f) {
+  if (!f.down) return;  // Inf
+  // if (!f.up) f.down = 1; return;  // 0      // debug01
+  if (!f.up) {
+    f.down = 1; return;
+  }
+  if (f.down < 0) {   // down<0
+    f.up *= -1;
+    f.down *= -1;
+  }
+  long long up_t = f.up, g = 0;
+  if (f.up < 0) up_t = -1 * f.up;
+  g = gcd(up_t, f.down);
+  if (g > 1) {
+    f.up /= g;
+    f.down /= g;
+  }
+  // g == 1 ok
+}
+
+Fraction add(Fraction a, Fraction b) {
+  Fraction f;
+  f.up = a.up * b.down + b.up * a.down;
+  f.down = a.down * b.down;
+  reduction(f);
+  return f;
+}
+
+Fraction minus(Fraction a, Fraction b) {
+  Fraction f;
+  f.up = a.up * b.down - b.up * a.down;
+  f.down = a.down * b.down;
+  reduction(f);
+  return f;
+}
+
+Fraction multi(Fraction a, Fraction b) {
+  Fraction f;
+  f.up = a.up * b.up;
+  f.down = a.down * b.down;
+  reduction(f);
+  return f;
+}
+
+Fraction divide(Fraction a, Fraction b) {
+  Fraction f;
+  f.up = a.up * b.down;
+  f.down = a.down * b.up;
+  reduction(f);
+  return f;
+}
+
+
+long long rabs(long long n) {
+  return n < 0 ? -n : n;
+}
+
+void show(Fraction f) {
+  if (!f.down) {
+    printf("Inf"); return;
+  }
+  if (!f.up) {
+    printf("0"); return;
+  }
+
+  if (f.up < 0) printf("(-");   // (-
+
+  if (f.down == 1) {  // ok->int
+    printf("%lld", rabs(f.up));
+    if (f.up < 0) printf(")");   // )
+    return;
+  }
+
+  if (rabs(f.up) < f.down) {     // no int, attetion
+    printf("%lld/%lld", rabs(f.up), f.down);
+  } else {
+    long long a = rabs(f.up) / f.down;
+    long long b = rabs(f.up) - a*f.down;
+    printf("%lld %lld/%lld", a, b, f.down);
+  }
+  if (f.up < 0) printf(")");     // )
+}
+
+int main() {
+  Fraction a, b;
+  scanf("%lld/%lld %lld/%lld", &a.up, &a.down, &b.up, &b.down);
+  reduction(a); reduction(b);
+  show(a); printf(" + "); show(b); printf(" = "); show(add(a, b)); printf("\n");
+  show(a); printf(" - "); show(b); printf(" = "); show(minus(a, b)); printf("\n");
+  show(a); printf(" * "); show(b); printf(" = "); show(multi(a, b)); printf("\n");
+  show(a); printf(" / "); show(b); printf(" = "); show(divide(a, b)); printf("\n");
+  return 0;
+}
+
+// 134WA
+
+/*
+2/3 -4/2
+2/3 + (-2 0/2) = (-1 2/6)
+2/3 - (-2 0/2) = 2 4/6
+2/3 * (-2 0/2) = (-1 2/6)
+2/3 / (-2 0/2) = 0 4/-12
+*/
+
+// debug01
+
+// 34WA
+
+// debug02 long long
+
+// ----- 以上20180907
+
+/*
 #include <iostream>
 #include <cstdio>
 #include <cmath>
@@ -50,6 +173,7 @@ int main() {
   }
   return 0;
 }
+*
 
 /*
 #include <cstdio>
