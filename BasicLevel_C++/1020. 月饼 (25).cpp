8c039b73ec5c8252a49ff0e735c067d51e06b9af
timtingwei <timtingwei@hotmail.com>
2018-09-07 23:32:28 +0800

    优先队列

--- BasicLevel_C++/1020. 月饼 (25).cpp
+++ BasicLevel_C++/1020. 月饼 (25).cpp
@@ -83,3 +83,57 @@ int main() {
 
 
 
+
+/*
+// 09:36， 9:58, 10:10
+#include <cstdio>
+#include <queue>
+#include <vector>
+using namespace std;
+
+struct food {
+  double scale, each_scale;
+  double cnt;
+};
+
+struct cmp {
+  bool operator() (const food &f1, const food &f2) {
+    return f1.each_scale < f2.each_scale;
+  }
+};
+
+bool db_equal(const double d1, const double d2) {
+  return -0.0005 < d1-d2 && d1-d2 < 0.00005;
+}
+
+int main() {
+  int N;
+  double need; scanf("%d %lf", &N, &need);
+  vector<food> v(N);
+  priority_queue<food, vector<food>, cmp> q;
+  for (int i = 0; i < N; i++) {
+    scanf("%lf", &v[i].cnt);
+  }
+  for (int i = 0; i < N; i++) {
+    scanf("%lf", &v[i].scale);
+    v[i].each_scale = v[i].scale / v[i].cnt;
+    q.push(v[i]);
+  }
+  double profit = 0.0;
+  while (!db_equal(need, 0.0)) {
+    if (q.top().cnt > need) {
+      profit += q.top().each_scale * need;
+      need = 0;
+    } else {  // <= need
+      profit += q.top().scale;
+      need -= q.top().cnt;
+      q.pop();
+    }
+    if (q.empty()) break;
+  }
+  printf("%.2lf", profit);
+  return 0;
+}
+
+// case2 error
+*/
