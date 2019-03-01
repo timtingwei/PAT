51db908af9b2c1b69c342dc1ffbebe171592298b
timtingwei <timtingwei@hotmail.com>
2019-03-01 18:16:46 +0800

    Create: 图的遍历

---
+++ AdvancedLevel_C++/1154-vertex-coloring-25.cpp
@@ -0,0 +1,63 @@
+/* Copyright [2019] <mituh> */
+/* 1154-vertex-coloring-25.cpp */
+/* 堆, 树的遍历 */
+
+#include <cstdio>
+#include <vector>
+using namespace std;
+#define MaxSize 10000
+#define INF 99999
+
+// vector<vector<int> > G(MaxSize);
+vector<int> G[MaxSize];
+
+int ok, cnt, color[MaxSize], have[MaxSize], visited[MaxSize] = {0};
+void dfs(int V) {
+  int i, W;
+  visited[V] = 1;
+  for (i = 0; i < G[V].size(); i++) {
+    W = G[V][i];
+    if (!visited[W]) {
+      if (color[W] != color[V]) {
+        dfs(W);
+      } else {
+        visited[W] = 1;
+        ok = 0;
+      }
+    }
+  }
+}
+int main() {
+#ifdef LOCAL
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+#endif
+  int n, m, i, j, V, W, c;
+  scanf("%d%d", &n, &m);
+  for (i = 0; i < m; i++) {
+    scanf("%d%d", &V, &W);
+    G[V].push_back(W); G[W].push_back(V);
+  }
+  int K; scanf("%d", &K);
+  for (i = 0; i < K; i++) {
+    fill(have, have+MaxSize, 0);
+    fill(visited, visited+MaxSize, 0);
+    fill(color, color+MaxSize, 0);
+    ok = 1; cnt = 0;
+    for (j = 0; j < n; j++) {
+      scanf("%d", &c);
+      color[j] = c;
+      if (!have[c]) {
+        cnt++;
+        have[color[j]] = 1;
+      }
+    }
+    dfs(0);
+    if (ok) {
+      printf("%d-coloring\n", cnt);
+    } else {
+      printf("No\n");
+    }
+  }
+  return 0;
+}
